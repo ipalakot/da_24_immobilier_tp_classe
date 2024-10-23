a89b2322a332a27a1ec25249be9ca315cc184302
ipalakot <ipalakot@gmail.com>
2024-10-23 03:55:21 +0200

    Article Controller

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/article', name: '')]
+//#[Route('/article', name: '')]
 class ArticleController extends AbstractController
 {
     #[Route('/', name: 'index')]
