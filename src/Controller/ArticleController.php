6ab27c5e22efbb16b53bbdb933a43d24e3ff9d7d
Igor Palakot <ipalakot@guinot.com>
2024-11-06 17:18:43 +0100

    Enregistrement

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+
+use App\Repository\ArticleRepository;
+
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,15 +16,68 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 //#[Route('/article', name: '')]
 class ArticleController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+       
+    
+    #[Route('/articles', name: 'article_index')]
+    public function index(ArticleRepository $articleRepository): Response
     {
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles'=> $articleRepository->findAll(),
         ]);
     }
 
+    #[Route('/articles/nouveau', name: 'article_nouveau')]
+    public function ajoutArticle(Request $request, EntityManagerInterface $manager)
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+        
+        // SIMPLIFICATION DE LA TACHE
+            ->add('titre')
+            ->add('adresse')
+            ->add('images')
+            ->add('type')
+            ->add('surface')
+            ->add('prix')
+            ->add('owner')
+            ->add('owner')
+            ->add('gestionnaire')
+            ->add('agence')
+            ->add('description')
+            ->getForm();
+
+        $form->handleRequest($request); // Le Request
+
+        //var_dump($article);
+
+        if ($form->isSubMitted() && $form->isValid()) { // Soumission du Formulaire
+            $manager->persist($article); // Persistancede mon article
+            $manager->flush(); // Enregistrement de l'article dans la BD
+
+            return $this->redirectToRoute('article_affichage', ['id' => $article->getId()]); // Redirection vers l'article
+        }
+
+        return $this->render('article/nouveau.html.twig', [
+            'formCreatArticle' => $form->createView(),
+        ]);
+    }
     
+
+    #[Route('/articles/{id}', name: 'article_affichage', methods: ['GET'])]
+    public function affichage(Article $article): Response
+    {
+        return $this->render('article/affichage.html.twig', [
+            'controller_name' => 'ArticleController',
+            'article'=> $article,
+        ]);
+    }
+
+
+    
+
+
+  
     public function contact(Request $request)
     {
         //dd('$contact');
