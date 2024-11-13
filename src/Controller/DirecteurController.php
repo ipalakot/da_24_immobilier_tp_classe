11d1f23ea1ceb2896cfba7e8a84c96235f56e560
Igor Palakot <ipalakot@guinot.com>
2024-11-13 14:05:17 +0100

    Directeur Modif

--- src/Controller/DirecteurController.php
+++ src/Controller/DirecteurController.php
@@ -12,14 +12,24 @@ use Symfony\Component\Routing\Attribute\Route;
 class DirecteurController extends AbstractController
 {
     #[Route('/directeur', name: 'app_directeur')]
-    public function index(): Response
+    public function index(directeurRepository $directeurRepository): Response
     {
         return $this->render('directeur/index.html.twig', [
             'controller_name' => 'DirecteurController',
+            'directeurs' => $directeurRepository->findAll()
         ]);
     }
 
-    #[Route('/directeurs/nouveau', name: 'directeur_nouveau')]
+    #[Route('/directeur/{id}', name: 'app_directeur')]
+    public function affichage(directeur $directeur): Response
+    {
+        return $this->render('directeur/affichage.html.twig', [
+            'controller_name' => 'DirecteurController',
+            'directeur' => $directeur,
+        ]);
+    }
+
+    #[Route('/directeur/nouveau', name: 'directeur_nouveau')]
     public function ajoutDirecteur(Request $request, EntityManagerInterface $manager)
     {
         $directeur = new Directeur();
@@ -35,6 +45,26 @@ class DirecteurController extends AbstractController
             return $this->redirectToRoute('directeur_affichage', ['id' => $directeur->getId()]);
         }
         return $this->render('directeur/nouveau.html.twig', 
+        [
+            'formCreatDirecteur' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/directeur/modif/{id}', name: 'directeur_modif')]
+    public function modifDirecteur(Directeur $directeur, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createFormBuilder($directeur)
+        ->add('nom')
+        ->add('prenom')
+        ->add('revenus')
+        ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubMitted() && $form->isValid()) {
+            $manager->persist($directeur);
+            $manager->flush();
+            return $this->redirectToRoute('directeur_affichage', ['id' => $directeur->getId()]);
+        }
+        return $this->render('directeur/modif.html.twig', 
         [
             'formCreatDirecteur' => $form->createView(),
         ]);
